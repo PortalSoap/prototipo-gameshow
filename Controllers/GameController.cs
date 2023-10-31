4c0c558be8072a1af63e295dbff11b3f4c9ec3e9
Lucas M. Lacerda <lucas.lacerda.lg@gmail.com>
2023-10-30 22:46:31 -0300

    Sistema de adição de temas implementado.
    O sistema de adição de temas foi implementado no projeto.

--- Controllers/GameController.cs
+++ Controllers/GameController.cs
@@ -1,11 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
 using ButtonBattle.Data;
+using ButtonBattle.Models;
+using System.Reflection.Metadata;
 
 namespace ButtonBattle.Controllers;
 
 public class GameController : Controller
 {
     private readonly ButtonBattleDbContext _dbContext;
+    private static int _count;
+    private static List<QuestionViewModel> _tempQuestionList = new List<QuestionViewModel>();
 
     public GameController(ButtonBattleDbContext dbContext)
     {
@@ -19,7 +23,9 @@ public class GameController : Controller
 
     public IActionResult Themes()
     {
-        return View();
+        List<String> themes = _dbContext.Questions.Select(x => x.Theme).Distinct().ToList();
+        _count = 0;
+        return View(themes);
     }
 
     public IActionResult NewTheme()
@@ -27,9 +33,66 @@ public class GameController : Controller
         return View();
     }
 
-    public IActionResult NewQuestion()
+    public IActionResult NewQuestion(string theme)
     {
-        return View();
+        if(_count <= 2)
+        {
+            return View("NewQuestion", theme);
+        }
+        else
+        {
+            return RedirectToAction("AddQuestions", "Game");
+        }
+        
+    }
+
+    public async Task<IActionResult> AddQuestions(string theme, string question, 
+    string optionA, string optionB, string optionC, string optionD, string optionE, 
+    string selected)
+    {
+        QuestionViewModel newQuestion = new QuestionViewModel
+        {
+            Theme = "Tema",
+            Question = "Pergunta",
+            OptionA = "123",
+            OptionB = "123",
+            OptionC = "123",
+            OptionD = "123",
+            OptionE = "123",
+            CorrectOption = "c"
+        };
+
+        if (_count <= 2)
+        {
+            newQuestion = new QuestionViewModel
+            {
+                Theme = theme,
+                Question = question,
+                OptionA = optionA,
+                OptionB = optionB,
+                OptionC = optionC,
+                OptionD = optionD,
+                OptionE = optionE,
+                CorrectOption = selected
+            };
+
+            _count++;
+            _tempQuestionList.Add(newQuestion);
+            return RedirectToAction("NewQuestion", "Game", new {theme = theme});
+        }
+        else
+        {
+            foreach(QuestionViewModel q in _tempQuestionList)
+            {
+                await _dbContext.Questions.AddAsync(q);
+            }
+            await _dbContext.SaveChangesAsync();
+            
+            _count = 0;
+            _tempQuestionList.Clear();
+
+            return RedirectToAction("Themes", "Game");
+        }
     }
 
     public IActionResult Question()
