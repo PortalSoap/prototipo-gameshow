325347993e203c4cea591df0480a92ca44f590b8
Lucas M. Lacerda <lucas.lacerda.lg@gmail.com>
2023-10-31 13:05:24 -0300

    Sistema de reconhecimento de clique implementado.
    Um sistema que reconhece qual entre dois jogadores foi o primeiro a clicar foi adicionado.

--- wwwroot/js/script.js
+++ wwwroot/js/script.js
@@ -1,5 +1,39 @@
-document.onkeydown = function (e) {
-    if(e.key == 'a' || e.key == 'l'){
-        console.log(e.key);
+document.addEventListener("DOMContentLoaded", function () {
+
+    let secondsRemaining = 3;
+    let timer = document.getElementById("start-timer");
+
+    function updateTimer() {
+        secondsRemaining--;
+        if (secondsRemaining < 1) {
+            clearInterval(timerInterval);
+            timer.style.display = "none";
+        }
+        timer.innerHTML = secondsRemaining;
     }
-}
+
+    let timerInterval = setInterval(updateTimer, 1000);
+
+    setTimeout(function () {
+        let content = document.getElementById("question-info");
+        content.style.display = "block";
+
+        let isDefined = false;
+        document.onkeydown = function (e) {
+            if (isDefined == false) {
+                if (e.key == 'a' || e.key == 'l') {
+                    console.log(e.key);
+                    isDefined = true;
+
+                    if (e.key == 'a') {
+                        let image = document.getElementById("player-1").src = "/img/man-answer.png";
+                    }
+                    else {
+                        let image = document.getElementById("player-2").src = "/img/woman-answer.png";
+                    }
+                }
+            }
+        }
+    }, 3000);
+})
+
